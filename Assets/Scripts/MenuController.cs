825363822229314264061205ff7996ee3fdd9530
emiInterBrain <emiliano.garcia@interbrainco.com>
2024-01-17 14:22:07 -0300

    first commit

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Drawing;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField]
+    InputField nameTxt;
+    [SerializeField]
+    Text bestScoreTxt;
+    public string name;
+    public int bestScore = 0;
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        LoadPrefs();
+    }
+    void Start()
+    {
+        ChangeBestScore(bestScore); 
+        bestScoreTxt.text = "best score:" + bestScore;
+    }
+    public void GetNameFromInputField()
+    {
+        name = nameTxt.text;
+        bestScore = ChangeBestScore(0);
+        bestScoreTxt.text = "BEST SCORE: " + " | " + bestScore;
+    }
+    public int ChangeBestScore(int newScore)
+    {
+        int score;
+        if (bestScore < newScore)
+        {
+            score = newScore;
+        }
+        else { score = bestScore; }
+        return score;
+    }
+    public int BestScore()
+    {
+        return bestScore;
+    }
+    public string NameSend()
+    {
+        return name;
+    }
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene("main");
+    }
+    public void ExitAplication()
+    {
+        Application.Quit();
+    }
+    public void SavePrefs(int bs, string nm)
+    {
+        PlayerPrefs.SetInt("bestScore", bs);
+        PlayerPrefs.SetString("name", nm);
+        PlayerPrefs.Save();
+    }
+
+    public void LoadPrefs()
+    {
+        bestScore = PlayerPrefs.GetInt("bestScore", bestScore);
+        name = PlayerPrefs.GetString("name", name);
+    }
+}
